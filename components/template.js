28e842d5411ebce24c14ad668138e66d360bbd3f
Hobgoblin101 <aj@bilby.net>
2017-06-28 09:36:04 +1000

    Implemented Template Component

--- components/template.js
+++ components/template.js
@@ -1,1 +1,71 @@
 var system = require('./system.js');
+
+
+
+
+function Merg(a, b){
+  isA = typeof(a) == "object";
+  isB = typeof(b) == "object";
+
+  if (!isA && !isB){
+    return null;
+  }else{
+    if (!isA && isB){
+      return isB;
+    }else if(isA && !isB){
+      return isA;
+    }
+  }
+
+  for (let key of b){
+    if (!a[key]){
+      a[key] = b[key];
+    }else{
+      if (typeof(b[key]) === "object" && typeof(a[key]) === "object"){
+        a[key] = Merg(a[key], b[key]);
+      }else{
+        a[key] = b[key];
+      }
+    }
+  }
+}
+
+
+
+
+
+function Get(type, callback){
+  if (!Exists(type)){
+    callback(null);
+    return;
+  }
+
+  system.readFile('./data/template/'+type+'.json', function(err, data){
+    if (data){
+      data = data.toString();
+      data = JSON.parse(data);
+      callback(data);
+    }else{
+      callback(null);
+    }
+  });
+}
+function Apply(type, data, callback){
+  Get(type, function(template){
+    if (data){
+      data = Merg(template, data);
+      callback(data);
+    }else{
+      callback(data);
+    }
+  });
+}
+function Exists(type){
+  return fs.existsSync('./data/template/'+type+'.json');
+}
+
+module.exports = {
+  get: Get,
+  apply: Apply,
+  exists: Exists
+};
